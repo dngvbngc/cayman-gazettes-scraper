dfe4a2636d77098612d3e9697528e82adc9dee45
dngvbngc <dvbngoc.0706@gmail.com>
2025-04-21 13:05:53 +0800

    Add backend

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, Response, send_file
+from scrape import scrape
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/generate-pdf', methods=['POST'])
+def generate_pdf():
+    token = request.form.get('token')
+    year = request.form.get('year')
+    year = int(year) if year else 2025
+    pdf_file = scrape(token, year)
+    return send_file(pdf_file, as_attachment = True, download_name = f'gazetts-{year}.pdf', mimetype="application/pdf")
+
+if __name__ == '__main__':
+	app.run()
--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 import requests
-
-years = [i for i in range(2026, 2019, -1)]
+from PyPDF2 import PdfMerger 
+from io import BytesIO
 
 years_id = {   
     2025: "EBEAC76BE72D429B8CB93DBC47CEB5BB",
@@ -17,12 +17,35 @@ years_limit = {
     2020: {"gt": "20191231", "lt": "20210101"},
 }
 
-# Scrape gazetts from Cayman Gazetts
-# 2 sources: Gazett page & Archive page
-# Collect channel token by manually accessing website
+# Unused
+def get_year_ids(token):
+    ids = []
+    url = 'https://www.gov.ky/gazettes/pages/48.json'
+    response = requests.get(url)
+    content = response.json()
+    body = content.get('base').get("slots").get('body')
+    components = body.get('components')
+    component_instances = content.get('base').get('componentInstances')
+    for c in components:
+        cat = component_instances.get(c).get("data").get("categoryFilters")
+        if cat:
+            id = cat[0].get("categories")[0]
+            ids.append(id)
 
-def scrape_year(token: str, year):
-    # From the main Gazett page, request for gazetts
+    return ids
+
+def scrape_year(token, year=2025):
+    """
+    Scrapes gazette attachments from the Cayman Islands Gazette API for a given year.
+    Channel token must be collected manually by accessing the Gazette website.
+
+    Args:
+        token (str): The channel token for API authentication.
+        year (int): The year for which to scrape gazettes. Defaults to 2025.
+
+    Returns:
+        List[str]: A list of attachment IDs corresponding to the gazette PDFs.
+    """
     base_url = "https://www.gov.ky/content/published/api/v1.1/items"
 
     query = f"""((type eq "Publication") and (language eq "en-GB" or translatable eq "false") and ((taxonomies.categories.nodes.id eq "{years_id.get(year)}"))"""
@@ -42,23 +65,43 @@ def scrape_year(token: str, year):
     response = requests.get(base_url, params = params)
     gazetts = response.json()
 
-    if str(gazetts.get('hasMore')) == 'True':
-        print('More gazetts to be found...')
-
-    # Extract the slugs from the json and go to each associated page to get pdf
-    slugs = []
+    # Extract the ids from the json to get pdf
+    attachments = []
     items = gazetts.get('items')
     for item in items:
-        slug = item.get('slug')
-        slugs.append(slug)
-    return slugs
+        attachment = item.get('fields').get('attachment')[0].get('id')
+        attachments.append(attachment)
+    return attachments
 
-def scrape(token: str):
-    slugs = []
-    for year in years:
-        year_slugs = scrape_year(token, year)
-        for s in year_slugs:
-            slugs.append(s)
-    return slugs
+def scrape(token, year):
+    """
+    Downloads and merges Cayman Islands Gazette PDFs for a given range of years.
 
-print(scrape('3d887eb9d03345709d279836a8be130e'))
+    Args:
+        token (str): Channel token for authenticating API requests.
+        start_year (int, optional): The starting year for scraping gazettes (inclusive). Defaults to 2020.
+        end_year (int, optional): The ending year for scraping gazettes (inclusive). Defaults to 2025.
+
+    Returns:
+        BytesIO: The merged PDF.
+    """
+    attachments = scrape_year(token, year)
+    merger = PdfMerger()
+
+    # From the ids, download the PDFs and combine
+    for a in attachments:
+        url = f'https://www.gov.ky/content/published/api/v1.1/items/{a}?channelToken={token}'
+        response = requests.get(url)
+        content = response.json()
+        pdf_url = content.get('fields').get('native').get('links')[0].get('href')
+        pdf_response = requests.get(pdf_url)
+        if pdf_response.status_code == 200:
+            pdf_file = BytesIO(pdf_response.content)
+            merger.append(pdf_file)
+
+    output_pdf = BytesIO()
+    merger.write(output_pdf)
+    merger.close()
+    output_pdf.seek(0)
+
+    return output_pdf
